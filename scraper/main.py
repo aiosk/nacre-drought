b5c7c55f5d9a7bee69f0f95b3185528779e68b45
Andry <andry.yosua@pinteraktif.com>
2017-04-26 12:53:43 +0700

    update algorithm to append pemilih if not exist

--- scraper/main.py
+++ scraper/main.py
@@ -1,5 +1,6 @@
 import requests
-
+import json
+from time import sleep
 
 def getData(location=''):
     url = 'https://pilkada2017.kpu.go.id/pemilih/dpt/2/DKI%20JAKARTA/{location}listDps.json'.format(
@@ -7,7 +8,11 @@ def getData(location=''):
     data = requests.get(url=url).json()
     return data['aaData'] if 'aaData' in data else data['data']
 
+dataFileName = 'data2.json'
 data = []
+with open(dataFileName) as f:
+    data = json.load(f)
+
 propinsi = getData()
 for kabKotaVal in propinsi:
     location = '{namaKabKota}/'.format(namaKabKota=kabKotaVal['namaKabKota'])
@@ -25,12 +30,15 @@ for kabKotaVal in propinsi:
                     'namaKabKota'], namaKecamatan=kecamatanVal['namaKecamatan'], namaKelurahan=kelurahanVal['namaKelurahan'], tps=tpsVal['tps'])
                 tps = getData(location)
                 for pemilih in tps:
-                    pemilih['kelurahan'] = kelurahanVal['namaKelurahan']
-                    pemilih['kecamatan'] = kecamatanVal['namaKecamatan']
-                    pemilih['kabkota'] = kabKotaVal['namaKabKota']
-                    print(pemilih)
-                    data.append(pemilih)
+                    if not any(dataPemilih['id'] == pemilih['id'] for dataPemilih in data):
+                        pemilih['kelurahan'] = kelurahanVal['namaKelurahan']
+                        pemilih['kecamatan'] = kecamatanVal['namaKecamatan']
+                        pemilih['kabkota'] = kabKotaVal['namaKabKota']
 
-import json
-with open('data.json', 'w') as outfile:
-    json.dump(data, outfile)
+                        print('append {nik} {nama} to file'.format(nik=pemilih['nik'],nama=pemilih['nama']))
+                        data.append(pemilih)
+                        with open(dataFileName, 'w') as f:
+                            json.dump(data, f)
+                        sleep(1)
+                    else:
+                        print('{nik} {nama} already exists'.format(nik=pemilih['nik'],nama=pemilih['nama']))
