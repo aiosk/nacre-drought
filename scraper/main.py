2ef76cb3e454867fb38463276d16fd40516734dc
Andry <andry.yosua@pinteraktif.com>
2017-04-27 19:18:32 +0700

    removing pemilih check if exists

--- scraper/main.py
+++ scraper/main.py
@@ -31,13 +31,13 @@ c.execute('''CREATE TABLE IF NOT EXISTS tempatLahir (
 
 # load data to memory
 data = []
-dataCheckExist = tuple(r[0] for r in c.execute('select id from pemilih').fetchall())
-rowKelurahan = c.execute('select id,nama from kelurahan').fetchall()
-rowKecamatan = c.execute('select id,nama from kecamatan').fetchall()
-rowKabkota = c.execute('select id,nama from kabkota').fetchall()
-rowJenisKelamin = c.execute('select id,nama from kelamin').fetchall()
-rowTempatLahir = c.execute('select id,nama from tempatLahir').fetchall()
-urlData = (r[0] for r in c.execute('select url from source').fetchall())
+dataCheckExist = tuple(r[0] for r in c.execute('SELECT id FROM pemilih').fetchall())
+rowKelurahan = c.execute('SELECT id,nama FROM kelurahan').fetchall()
+rowKecamatan = c.execute('SELECT id,nama FROM kecamatan').fetchall()
+rowKabkota = c.execute('SELECT id,nama FROM kabkota').fetchall()
+rowJenisKelamin = c.execute('SELECT id,nama FROM kelamin').fetchall()
+rowTempatLahir = c.execute('SELECT id,nama FROM tempatLahir').fetchall()
+urlData = tuple(r[0] for r in c.execute('SELECT url FROM source').fetchall())
 
 
 def getData(url=''):
@@ -48,41 +48,54 @@ def getData(url=''):
 
 
 def getTempatLahir(tempatLahirPemilih):
-    tempatLahir = [r[0] for r in rowTempatLahir if tempatLahirPemilih.upper() == r[1]]
+    global rowTempatLahir
+
+    tempatLahir = tuple( r[0] for r in rowTempatLahir if tempatLahirPemilih.upper() == r[1])
     if not tempatLahir:
-        tempatLahir = c.execute('''INSERT INTO tempatLahir (nama) VALUES (?)''', (tempatLahirPemilih.upper(),)).lastrowid
+        tempatLahir = c.execute( 'INSERT INTO tempatLahir (nama) VALUES (?)', (tempatLahirPemilih.upper(),)).lastrowid
+        rowTempatLahir = c.execute( 'SELECT id,nama FROM tempatLahir').fetchall()
     else:
         tempatLahir = tempatLahir[0]
 
     return tempatLahir
 
 
-def preparePemilih(pemilih, url):
-    jenisKelamin = [r[0] for r in rowJenisKelamin if pemilih['jenisKelamin'].lower() == r[1]][0]
+def parseLocation(url):
+    location = url.split('/')
+    kelurahan = tuple(r[0] for r in rowKelurahan if location[-3].upper() == r[1])[0]
+    kecamatan = tuple(r[0] for r in rowKecamatan if location[-4].upper() == r[1])[0]
+    kabkota = tuple(r[0] for r in rowKabkota if location[-5].upper() == r[1])[0]
+
+    return {'kelurahan': kelurahan, 'kecamatan': kecamatan, 'kabkota': kabkota}
+
+
+def preparePemilih(pemilih):
+    jenisKelamin = tuple(r[0] for r in rowJenisKelamin if pemilih[ 'jenisKelamin'].lower() == r[1])[0]
     tempatLahir = getTempatLahir(pemilih['tempatLahir'])
 
-    location = url.split('/')
-    kelurahan = [r[0] for r in rowKelurahan if location[-3].upper() == r[1]][0]
-    kecamatan = [r[0] for r in rowKecamatan if location[-4].upper() == r[1]][0]
-    kabkota = [r[0] for r in rowKabkota if location[-5].upper() == r[1]][0]
-
-    return (pemilih['id'], pemilih['nama'].upper(), pemilih['nik'], tempatLahir, pemilih['tps'], pemilih['putaran'], jenisKelamin, kelurahan, kecamatan, kabkota,)
+    return (pemilih['id'], pemilih['nama'].upper(), pemilih['nik'], tempatLahir, pemilih['tps'], pemilih['putaran'], jenisKelamin, pemilih['kelurahan'], pemilih['kecamatan'], pemilih['kabkota'],)
 
 
 try:
     for urlVal in urlData:
         # sleep(1)
         tps = getData(urlVal)
+        tpsLocation = parseLocation(urlVal.replace('%20', ' '))
 
         for pemilih in tps:
             if pemilih['id'] in dataCheckExist:
                 print('Pemilih {nik} {nama} already exists'.format(nik=pemilih['nik'], nama=pemilih['nama']))
             else:
+                pemilih.update(tpsLocation)
                 print('Append {nik} {nama} to data pemilih'.format(nik=pemilih['nik'], nama=pemilih['nama']))
-                pemilih = preparePemilih(pemilih, urlVal.replace('%20', ' '))
+                pemilih = preparePemilih(pemilih)
 
-                # print(pemilih)
-                data.append(pemilih)
+            # pemilih.update(tpsLocation)
+            # print('Append {nik} {nama} to data pemilih'.format( nik=pemilih['nik'], nama=pemilih['nama']))
+            # pemilih = preparePemilih(pemilih)
+
+            # print(pemilih)
+            data.append(pemilih)
 
         print('removing {url} from list'.format(url=urlVal))
         c.execute('DELETE FROM source where url=?', (urlVal,))
@@ -95,5 +108,3 @@ finally:
         VALUES  ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', tuple(data))
     conn.commit()
     conn.close()
-
-    # sys.exit(0)
