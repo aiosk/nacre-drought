5810f350169258a5d65b183398c6eda93aee05d8
Andry <andry.yosua@pinteraktif.com>
2017-04-28 11:06:29 +0700

    normalize database

--- scraper/main.py
+++ scraper/main.py
@@ -7,107 +7,92 @@ conn = sqlite3.connect('dpt.db')
 c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS pemilih (
-    id INTEGER NOT NULL UNIQUE ON CONFLICT IGNORE,
-    nama varchar(100) NOT NULL,
-    nik varchar(20) NOT NULL,
-    tempatLahir varchar(50) NOT NULL,
-    tps varchar(4) NOT NULL,
-    putaran varchar(2) NOT NULL,
-    jenisKelamin INTEGER NOT NULL,
-    kelurahan INTEGER NOT NULL,
-    kecamatan INTEGER NOT NULL,
-    kabkota INTEGER NOT NULL
+    id INTEGER NOT NULL,
+    nama VARCHAR(100) NOT NULL,
+    nik VARCHAR(20) NOT NULL,
+    putaran INTEGER NOT NULL,
+    jenisKelaminId INTEGER NOT NULL,
+    tempatLahirId INTEGER NOT NULL,
+    tpsId INTEGER NOT NULL,
+    UNIQUE (id, nama, putaran, jenisKelaminId, tempatLahirId, tpsId),
+    FOREIGN KEY (jenisKelaminId) REFERENCES jenisKelamin(id),
+    FOREIGN KEY (tempatLahirId) REFERENCES tempatLahir(id),
+    FOREIGN KEY (tpsId) REFERENCES tps(id)
 )''')
-c.execute('''CREATE TABLE IF NOT EXISTS kelamin (
+c.execute('''CREATE TABLE IF NOT EXISTS jenisKelamin (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nama varchar(100) NOT NULL UNIQUE ON CONFLICT IGNORE
+    nama VARCHAR(100) NOT NULL UNIQUE
 )''')
-c.executemany('INSERT INTO kelamin (nama) VALUES (?)',
-              (('laki-laki',), ('perempuan',)))
-c.execute('''CREATE TABLE IF NOT EXISTS tempatLahir (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  nama varchar(100) NOT NULL UNIQUE ON CONFLICT IGNORE
-)''')
-
-# load data to memory
-data = []
-dataCheckExist = set(r[0] for r in c.execute('SELECT id FROM pemilih').fetchall())
-rowKelurahan = c.execute('SELECT id,nama FROM kelurahan').fetchall()
-rowKecamatan = c.execute('SELECT id,nama FROM kecamatan').fetchall()
-rowKabkota = c.execute('SELECT id,nama FROM kabkota').fetchall()
-rowJenisKelamin = c.execute('SELECT id,nama FROM kelamin').fetchall()
-rowTempatLahir = c.execute('SELECT id,nama FROM tempatLahir').fetchall()
-urlData = set(r[0] for r in c.execute('SELECT url FROM source').fetchall())
-
-
-def getData(url=''):
-    print('processing {url}'.format(url=url))
-
-    data = requests.get(url=url).json()
-    return data['data']
-
-
-def getTempatLahir(tempatLahirPemilih):
-    global rowTempatLahir
-
-    tempatLahir = tuple( r[0] for r in rowTempatLahir if tempatLahirPemilih.upper() == r[1])
-    if not tempatLahir:
-        tempatLahir = c.execute( 'INSERT INTO tempatLahir (nama) VALUES (?)', (tempatLahirPemilih.upper(),)).lastrowid
-        rowTempatLahir = c.execute( 'SELECT id,nama FROM tempatLahir').fetchall()
-    else:
-        tempatLahir = tempatLahir[0]
-
-    return tempatLahir
-
-
-def parseLocation(url):
-    location = url.split('/')
-    kelurahan = tuple(r[0] for r in rowKelurahan if location[-3].upper() == r[1])[0]
-    kecamatan = tuple(r[0] for r in rowKecamatan if location[-4].upper() == r[1])[0]
-    kabkota = tuple(r[0] for r in rowKabkota if location[-5].upper() == r[1])[0]
-
-    return {'kelurahan': kelurahan, 'kecamatan': kecamatan, 'kabkota': kabkota}
-
-
-def preparePemilih(pemilih):
-    jenisKelamin = tuple(r[0] for r in rowJenisKelamin if pemilih[ 'jenisKelamin'].lower() == r[1])[0]
-    tempatLahir = getTempatLahir(pemilih['tempatLahir'])
-
-    return (pemilih['id'], pemilih['nama'].upper(), pemilih['nik'], tempatLahir, pemilih['tps'], pemilih['putaran'], jenisKelamin, pemilih['kelurahan'], pemilih['kecamatan'], pemilih['kabkota'],)
-
 
 try:
+    c.executemany('INSERT INTO jenisKelamin (nama) VALUES (?)', (('laki-laki',), ('perempuan',)))
+except sqlite3.IntegrityError:
+    pass
+
+c.execute('''CREATE TABLE IF NOT EXISTS tempatLahir (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  nama varchar(100) NOT NULL UNIQUE
+)''')
+
+
+rowTempatLahir = c.execute('SELECT id,nama FROM tempatLahir').fetchall()
+def getTempatLahirId(tempatLahirPemilih):
+    global rowTempatLahir
+    try:
+        tempatLahirId = tuple( r[0] for r in rowTempatLahir if tempatLahirPemilih.upper() == r[1])[0]
+    except IndexError:
+        tempatLahirId = c.execute( 'INSERT INTO tempatLahir (nama) VALUES (?)', (tempatLahirPemilih.upper(),)).lastrowid
+        rowTempatLahir = c.execute( 'SELECT id,nama FROM tempatLahir').fetchall()
+
+    return tempatLahirId
+
+
+rowKelurahan = c.execute('SELECT id,nama FROM kelurahan').fetchall()
+rowTps = c.execute('SELECT id,nama,kelurahanId FROM tps').fetchall()
+def getTpsId(url):
+    location = url.split('/')
+    kelurahanId = tuple(r[0] for r in rowKelurahan if location[-3].upper() == r[1])[0]
+    tpsId = tuple(r[0] for r in rowTps if int(location[-2]) == r[1] and kelurahanId == r[2])[0]
+
+    return tpsId
+
+
+rowJenisKelamin = c.execute('SELECT id,nama FROM jenisKelamin').fetchall()
+def preparePemilih(pemilih, tpsId):
+    jenisKelaminId = tuple(r[0] for r in rowJenisKelamin if pemilih[ 'jenisKelamin'].lower() == r[1])[0]
+    tempatLahirId = getTempatLahirId(pemilih['tempatLahir'])
+
+    return (pemilih['id'], pemilih['nama'].upper(), pemilih['nik'], int(pemilih['putaran']), jenisKelaminId, tempatLahirId, tpsId,)
+
+
+data = []
+dataCheckExist = set(r[0] for r in c.execute('SELECT id FROM pemilih').fetchall())
+urlData = set(r[0] for r in c.execute( 'SELECT url FROM _source WHERE isFetched=0').fetchall())
+try:
     for urlVal in urlData:
-        # sleep(1)
-        tps = getData(urlVal)
-        tpsLocation = parseLocation(urlVal.replace('%20', ' '))
+        tps = requests.get(url=urlVal).json()['data']
+        tpsId = getTpsId(urlVal.replace('%20', ' '))
 
         for pemilih in tps:
             if pemilih['id'] in dataCheckExist:
-                print('Pemilih {nik} {nama} already exists'.format(nik=pemilih['nik'], nama=pemilih['nama']))
+                # print('Pemilih {nik} {nama} already exists'.format( nik=pemilih['nik'], nama=pemilih['nama']))
+                pass
             else:
-                pemilih.update(tpsLocation)
-                print('Append {nik} {nama} to data pemilih'.format(nik=pemilih['nik'], nama=pemilih['nama']))
-                pemilih = preparePemilih(pemilih)
+                # print('Append {nik} {nama} to data pemilih'.format( nik=pemilih['nik'], nama=pemilih['nama']))
+                print(pemilih)
+                pemilih = preparePemilih(pemilih, tpsId)
 
-                # print(pemilih)
                 data.append(pemilih)
 
-            # pemilih.update(tpsLocation)
-            # print('Append {nik} {nama} to data pemilih'.format( nik=pemilih['nik'], nama=pemilih['nama']))
-            # pemilih = preparePemilih(pemilih)
-
-            # print(pemilih)
-            # data.append(pemilih)
-
         print('removing {url} from list'.format(url=urlVal))
-        c.execute('DELETE FROM source where url=?', (urlVal,))
+        c.execute('UPDATE _source set isFetched=1 where url=?', (urlVal,))
 except KeyboardInterrupt:
     pass
 finally:
     print('saving pemilih to database')
+
     c.executemany('''INSERT INTO
-        pemilih (id, nama, nik, tempatLahir, tps, putaran, jenisKelamin, kelurahan, kecamatan, kabkota)
-        VALUES  ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)''', tuple(data))
+        pemilih (id, nama, nik, putaran, jenisKelaminId, tempatLahirId, tpsId)
+        VALUES  ( ?,    ?,   ?,       ?,              ?,             ?,     ?)''', tuple(data))
     conn.commit()
     conn.close()
--- scraper/url.py
+++ scraper/url.py
@@ -4,66 +4,98 @@ import sqlite3
 conn = sqlite3.connect('dpt.db')
 c = conn.cursor()
 
-c.execute('''CREATE TABLE IF NOT EXISTS source (
-    url VARCHAR(160) NOT NULL UNIQUE ON CONFLICT IGNORE
-)''')
-c.execute('''CREATE TABLE IF NOT EXISTS kelurahan (
+c.execute('''CREATE TABLE IF NOT EXISTS kabkota (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nama varchar(100) NOT NULL UNIQUE ON CONFLICT IGNORE
+    nama varchar(100) NOT NULL UNIQUE
 )''')
 c.execute('''CREATE TABLE IF NOT EXISTS kecamatan (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nama varchar(100) NOT NULL UNIQUE ON CONFLICT IGNORE
+    nama varchar(100) NOT NULL UNIQUE ,
+    kabkotaId INTEGER,
+    FOREIGN KEY (kabkotaId) REFERENCES kabkota(id)
 )''')
-c.execute('''CREATE TABLE IF NOT EXISTS kabkota (
+c.execute('''CREATE TABLE IF NOT EXISTS kelurahan (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nama varchar(100) NOT NULL UNIQUE ON CONFLICT IGNORE
+    nama varchar(100) NOT NULL UNIQUE ,
+    kecamatanId INTEGER,
+    FOREIGN KEY (kecamatanId) REFERENCES kecamatan(id)
+)''')
+c.execute('''CREATE TABLE IF NOT EXISTS tps (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nama INTEGER NOT NULL,
+    kelurahanId INTEGER,
+    UNIQUE (nama, kelurahanId),
+    FOREIGN KEY (kelurahanId) REFERENCES kelurahan(id)
+)''')
+c.execute('''CREATE TABLE IF NOT EXISTS _source (
+    url VARCHAR(160) NOT NULL UNIQUE ON CONFLICT IGNORE,
+    isFetched BOOLEAN NOT NULL DEFAULT 0,
+    tpsId INTEGER,
+    FOREIGN KEY (tpsId) REFERENCES tps(id)
 )''')
 
 
 def getUrl(location=''):
-    return 'https://pilkada2017.kpu.go.id/pemilih/dpt/2/DKI%20JAKARTA/{location}listDps.json'.format(
-        location=location).replace(' ', '%20')
+    return 'https://pilkada2017.kpu.go.id/pemilih/dpt/2/DKI%20JAKARTA/{location}listDps.json'.format( location=location).replace(' ', '%20')
 
 
 def getData(location=''):
-    url = getUrl(location)
-
-    print('fetching /{location}'.format(location=location))
-    data = requests.get(url=url).json()
+    data = requests.get(url=getUrl(location)).json()
 
     return data['aaData'] if 'aaData' in data else data['data']
 
+
+def getLocationId(table, value, fieldParent=None, parentId=None):
+    if not fieldParent:
+        try:
+            rowId = c.execute('INSERT INTO {table} (nama) VALUES (?)'.format( table=table), (value.upper(),)).lastrowid
+        except sqlite3.IntegrityError:
+            rowId = c.execute('SELECT id FROM {table} WHERE nama=?'.format( table=table), (value.upper(),)).fetchone()[0]
+    else:
+        try:
+            sql = 'INSERT INTO {table} (nama, {fieldParent}) VALUES (?,?)'.format( table=table, fieldParent=fieldParent)
+            rowId = c.execute(sql, (value.upper(), parentId,)).lastrowid
+        except sqlite3.IntegrityError:
+            sql = 'SELECT id FROM {table} WHERE nama=? and {fieldParent}=?'.format( table=table, fieldParent=fieldParent)
+            rowId = c.execute(sql, (value.upper(), parentId,)).fetchone()[0]
+
+    return rowId
+
 data = []
-propinsi = getData()
-for kabKotaVal in propinsi:
-    location = '{namaKabKota}/'.format(
-        namaKabKota=kabKotaVal['namaKabKota'])
-    c.execute('INSERT INTO kabkota (nama) VALUES (?)',
-              (kabKotaVal['namaKabKota'].strip().upper(),))
-    kabKota = getData(location)
+try:
+    propinsi = getData()
+    for kabKotaVal in propinsi:
+        location = '{namaKabKota}/'.format( namaKabKota=kabKotaVal['namaKabKota'])
+        kabkotaId = getLocationId('kabkota', kabKotaVal[ 'namaKabKota'].strip().upper())
 
-    for kecamatanVal in kabKota:
-        location = '{namaKabKota}/{namaKecamatan}/'.format(
-            namaKabKota=kabKotaVal['namaKabKota'], namaKecamatan=kecamatanVal['namaKecamatan'])
-        c.execute('INSERT INTO kecamatan (nama) VALUES (?)',
-                  (kecamatanVal['namaKecamatan'].strip().upper(),))
-        kecamatan = getData(location)
+        kabKota = getData(location)
+        for kecamatanVal in kabKota:
+            location = '{namaKabKota}/{namaKecamatan}/'.format( namaKabKota=kabKotaVal['namaKabKota'], namaKecamatan=kecamatanVal['namaKecamatan'])
+            kecamatanId = getLocationId('kecamatan', kecamatanVal[ 'namaKecamatan'].strip().upper(), 'kabkotaId', kabkotaId)
 
-        for kelurahanVal in kecamatan:
-            location = '{namaKabKota}/{namaKecamatan}/{namaKelurahan}/'.format(namaKabKota=kabKotaVal[
-                'namaKabKota'], namaKecamatan=kecamatanVal['namaKecamatan'], namaKelurahan=kelurahanVal['namaKelurahan'])
-            c.execute('INSERT INTO kelurahan (nama) VALUES (?)',
-                      (kelurahanVal['namaKelurahan'].strip().upper(),))
-            kelurahan = getData(location)
+            kecamatan = getData(location)
+            for kelurahanVal in kecamatan:
+                location = '{namaKabKota}/{namaKecamatan}/{namaKelurahan}/'.format(namaKabKota=kabKotaVal[ 'namaKabKota'], namaKecamatan=kecamatanVal['namaKecamatan'], namaKelurahan=kelurahanVal['namaKelurahan'])
+                kelurahanId = getLocationId('kelurahan', kelurahanVal[ 'namaKelurahan'].strip().upper(), 'kecamatanId', kecamatanId)
 
-            for tpsVal in kelurahan:
-                location = '{namaKabKota}/{namaKecamatan}/{namaKelurahan}/{tps}/'.format(namaKabKota=kabKotaVal[
-                    'namaKabKota'], namaKecamatan=kecamatanVal['namaKecamatan'], namaKelurahan=kelurahanVal['namaKelurahan'], tps=tpsVal['tps'])
-                url = getUrl(location)
-                print(url)
-                data.append((url,))
+                kelurahan = getData(location)
+                for tpsVal in kelurahan:
+                    location = '{namaKabKota}/{namaKecamatan}/{namaKelurahan}/{tps}/'.format(namaKabKota=kabKotaVal['namaKabKota'], namaKecamatan=kecamatanVal[ 'namaKecamatan'], namaKelurahan=kelurahanVal['namaKelurahan'], tps=tpsVal['tps'])
 
-c.executemany('INSERT INTO source (url) VALUES (?)', data)
-conn.commit()
-conn.close()
+                    try:
+                        c.execute('INSERT INTO tps (nama, kelurahanId) VALUES (?, ?)', (tpsVal[ 'tps'], kelurahanId,)).lastrowid
+                        print('Append TPS {kabkota} > {kecamatan} > {kelurahan} > {tps}'.format(kabkota=kabKotaVal[ 'namaKabKota'], kecamatan=kecamatanVal['namaKecamatan'], kelurahan=kelurahanVal['namaKelurahan'], tps=tpsVal['tps'],))
+                        data.append((getUrl(location), tpsVal['tps'],))
+                    except sqlite3.IntegrityError:
+                        print('TPS existed {kabkota} > {kecamatan} > {kelurahan} > {tps}'.format(kabkota=kabKotaVal[ 'namaKabKota'], kecamatan=kecamatanVal['namaKecamatan'], kelurahan=kelurahanVal['namaKelurahan'], tps=tpsVal['tps'],))
+
+except KeyboardInterrupt:
+    print('Handling KeyboardInterrupt exception')
+finally:
+    print('saving to database')
+
+    c.executemany('''INSERT INTO
+        _source (url, tpsId)
+         VALUES (  ?,     ?)''', tuple(data))
+    conn.commit()
+    conn.close()
