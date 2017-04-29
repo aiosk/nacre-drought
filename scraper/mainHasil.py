d67ebe08313c597df84534e91f59f289bbb617dc
Andry Yosua <andry.yosua@gmail.com>
2017-04-29 17:27:23 +0700

    add support for hasil / tps

--- scraper/mainHasil.py
+++ scraper/mainHasil.py
@@ -8,54 +8,117 @@ c = conn.cursor()
 
 c.execute('''CREATE TABLE IF NOT EXISTS suara (
     putaran INTEGER NOT NULL,
-    totalSuara INTEGER NOT NULL,
-    penggunaSuara INTEGER NOT NULL,
-    suaraSah INTEGER NOT NULL,
-    _suaraTidakSah INTEGER NOT NULL,
-    tpsId INTEGER NOT NULL UNIQUE,
+    total INTEGER NOT NULL,
+    pengguna INTEGER NOT NULL,
+    sah INTEGER NOT NULL,
+    _tidakSah INTEGER NOT NULL,
+    tpsId INTEGER NOT NULL,
+    UNIQUE (putaran, tpsId),
     FOREIGN KEY (tpsId) REFERENCES tps(id)
 )''')
 
+c.execute('''CREATE TABLE IF NOT EXISTS paslon (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    noUrut INTEGER NOT NULL,
+    kepalaDaerah VARCHAR(50) NOT NULL,
+    wakilKepalaDaerah VARCHAR(50) NOT NULL,
+    UNIQUE (noUrut, kepalaDaerah)
+)''')
+
+try:
+    data = []
+    data.append((1,'Agus Harimurti Yudhoyono, M.Sc., M.P.A., M.A.','Prof. Dr. Hj. Sylviana Murni, S.H.,M.Si.'))
+    data.append((2,'Ir. Basuki Tjahaja Purnama, M.M.','Drs. H. Djarot Saiful Hidayat, M.S.'))
+    data.append((3,'Anies Baswedan, Ph.D.','Sandiaga Salahuddin Uno'))
+    c.executemany('''INSERT INTO
+        paslon (noUrut,kepalaDaerah,wakilKepalaDaerah)
+        VALUES (     ?,           ?,                ?)''', tuple(data))
+except sqlite3.IntegrityError:
+    pass
+
+c.execute('''CREATE TABLE IF NOT EXISTS pilihan (
+    putaran INTEGER NOT NULL,
+    paslonId INTEGER NOT NULL,
+    total INTEGER NOT NULL DEFAULT 0,
+    tpsId INTEGER NOT NULL,
+    UNIQUE (putaran,paslonId, tpsId),
+    FOREIGN KEY (tpsId) REFERENCES tps(id),
+    FOREIGN KEY (paslonId) REFERENCES paslon(id)
+)''')
+
+
 import lxml.html
 from lxml.cssselect import CSSSelector
+import re
 urlData = set(r[0] for r in c.execute( 'SELECT url FROM _sourceHasil WHERE isFetched=0').fetchall())
 rowTps = c.execute('''SELECT t.id,t.nama,t.kelurahanId,k.nama kelurahanNama
     FROM tps t
     join kelurahan k on k.id=t.kelurahanId''').fetchall()
+
+rowPaslon = c.execute('SELECT id,noUrut FROM paslon').fetchall()
+paslon1Id=tuple(r[0] for r in rowPaslon if 1 == r[1])[0]
+paslon2Id=tuple(r[0] for r in rowPaslon if 2 == r[1])[0]
+paslon3Id=tuple(r[0] for r in rowPaslon if 3 == r[1])[0]
+
 try:
     for urlVal in urlData:
         page = requests.get(url=urlVal)
         page = lxml.html.fromstring(page.content)
-        tdSelector = CSSSelector('#rekapHasilPilkada td.text-center td.text-right')
 
+        print(urlVal)
         urlSplit = urlVal.split('/')
-        putaran = urlSplit[-6]
+        putaran = int(urlSplit[-6])
         kelurahanNama = urlSplit[-1].upper().replace('_',' ')
-        for i,tdElement in enumerate(tdSelector(page)):
+
+        tdSelectorSuara = CSSSelector('#rekapHasilPilkada td.text-center td.text-right')
+        for i,val in enumerate(tdSelectorSuara(page)):
             try:
                 if i%6==0:
-                    totalSuara= tdElement.text
+                    totalSuara= val.text
                 if i%6==1:
-                    penggunaSuara= tdElement.text
+                    penggunaSuara= val.text
                 if i%6==3:
-                    suaraSah= tdElement.text
+                    suaraSah= val.text
                 if i%6==4:
-                    _suaraTidakSah= tdElement.text
+                    _suaraTidakSah= val.text
 
                 if ((i+1)/6).is_integer():
                     tps= int(i/6)+1
                     tpsId = tuple(r[0] for r in rowTps if tps == r[1] and kelurahanNama == r[3])[0]
                     c.execute('''INSERT INTO
-                        suara  (putaran, totalSuara, penggunaSuara, suaraSah, _suaraTidakSah, tpsId)
+                        suara  (putaran, total, pengguna, sah, _tidakSah, tpsId)
                         VALUES (      ?,          ?,             ?,        ?,              ?,     ?)''', (putaran, totalSuara, penggunaSuara, suaraSah, _suaraTidakSah, tpsId,))
-                    print('Insert {kelurahanNama} > {tps} to data suara'.format(kelurahanNama=kelurahanNama,tps=tps))
+                    print('Insert suara {kelurahanNama} > {tps} to data suara'.format(kelurahanNama=kelurahanNama,tps=tps))
             except sqlite3.IntegrityError as e:
                 if 'unique constraint' in e.args[0].lower():
-                    print('{kelurahanNama} > {tps} already exists'.format(kelurahanNama=kelurahanNama,tps=tps))
+                    print('Suara {kelurahanNama} > {tps} already exists'.format(kelurahanNama=kelurahanNama,tps=tps))
                 else:
                     raise
 
-        c.execute('UPDATE _sourceHasil set isFetched=1 where url=?', (urlVal,))
+        tdSelectorPilihan = CSSSelector('#rekapHasilPilkada > tbody>tr>td:nth-child(4) script')
+        regexPaslonNo1 = re.compile(r".+case 1.+rekapHasil\.push\((\d+)\).+",re.MULTILINE)
+        regexPaslonNo2 = re.compile(r".+case 2.+rekapHasil\.push\((\d+)\).+",re.MULTILINE)
+        regexPaslonNo3 = re.compile(r".+case 3.+rekapHasil\.push\((\d+)\).+",re.MULTILINE)
+        for i,val in enumerate(tdSelectorPilihan(page)):
+            try:
+                tps=i+1
+                tpsId = tuple(r[0] for r in rowTps if tps == r[1] and kelurahanNama == r[3])[0]
+                paslon1=int([r.groups() for r in regexPaslonNo1.finditer(val.text)][0][0])
+                paslon2=int([r.groups() for r in regexPaslonNo2.finditer(val.text)][0][0])
+                paslon3=int([r.groups() for r in regexPaslonNo3.finditer(val.text)][0][0])
+                data = [(putaran,paslon1Id,paslon1,tpsId,), (putaran,paslon2Id,paslon2,tpsId,), (putaran,paslon3Id,paslon3,tpsId,)]
+
+                c.executemany('''INSERT INTO
+                    pilihan (putaran, paslonId, total, tpsId)
+                    VALUES  (      ?,      ?,     ?,     ?)''',tuple(data))
+                print('Insert pilihan {kelurahanNama} > {tps} to data pilihan'.format(kelurahanNama=kelurahanNama,tps=tps))
+            except sqlite3.IntegrityError as e:
+                if 'unique constraint' in e.args[0].lower():
+                    print('Pilihan {kelurahanNama} > {tps} already exists'.format(kelurahanNama=kelurahanNama,tps=tps))
+                else:
+                    raise
+
+        c.execute('UPDATE _sourceHasil SET isFetched=1 WHERE url=?', (urlVal,))
 except KeyboardInterrupt:
     pass
 finally:
