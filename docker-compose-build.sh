1e3d4fa801a75afb245beabf193b181cb7c86a24
Andry Yosua <andry.yosua@gmail.com>
2017-05-01 15:30:04 +0700

    add golang support

--- docker-compose-build.sh
+++ docker-compose-build.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
+FILE="go1.8.1.linux-armv6l.tar.gz"
+
+if [ ! -f "./scraper/$FILE" ]; then
+  curl -Lk -o "./scraper/$FILE" "https://storage.googleapis.com/golang/$FILE"
+fi
+
+
 docker-compose build
